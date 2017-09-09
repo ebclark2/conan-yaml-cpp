17fcba893a162dc0201848e89f692793f44042dc
ebclark2 <ebclark2@users.noreply.github.com>
2017-09-08 22:34:05 -0400

    Fix for VS

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,12 +7,13 @@ channel = os.getenv( 'CONAN_CHANNEL', 'testing' )
 class YamlCppTestConan( ConanFile ):
   settings = 'os', 'compiler', 'build_type', 'arch'
   requires = 'yaml-cpp/master@%s/%s' %  (username, channel )
+  build_requires = "cmake_installer/1.0@conan/stable"
   generators = 'cmake'
 
   def build( self ):
-    cmake = CMake( self.settings )
-    self.run( 'cmake "%s" %s' % ( self.conanfile_directory, cmake.command_line ) )
-    self.run( 'cmake --build . %s' % cmake.build_config )
+    cmake = CMake(self)
+    cmake.configure(source_dir=self.conanfile_directory)
+    cmake.build()
 
   def imports( self ):
     self.copy( '*.dll', 'bin', 'bin' )
