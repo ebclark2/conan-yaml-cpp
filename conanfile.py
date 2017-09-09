7d62c1ec16230251f966dd3c4d61054c8da9ca85
ebclark2 <ebclark2@users.noreply.github.com>
2017-09-08 21:44:31 -0400

    Added cmake build requirement

--- conanfile.py
+++ conanfile.py
@@ -7,6 +7,7 @@ class YamlcppConan(ConanFile):
     license = "<Put the package license here>"
     url = "<Package recipe repository url here, for issues about the package>"
     description = "<Description of Yamlcpp here>"
+    build_requires = "cmake_installer/1.0@conan/stable"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = "shared=False", "fPIC=False"
